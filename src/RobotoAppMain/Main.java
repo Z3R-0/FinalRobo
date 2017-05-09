a5c7ea7999d2dc3580fa3c3c7d915bfa6c652b2c
TimM <timmeermans@live.nl>
2017-05-09 16:14:34 +0200

    Figuring out the programming with JavaFX

--- src/RobotoAppMain/Main.java
+++ src/RobotoAppMain/Main.java
@@ -4,11 +4,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import javax.swing.*;
 
 public class Main extends Application {
+    private Stage primaryStage;
+    private BorderPane mainLayout;
+
 
     @Override
     public void start(Stage primaryStage) throws Exception {
