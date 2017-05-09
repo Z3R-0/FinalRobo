05bc06aa8543db0f9707797c484d053a8de12ff8
TimM <timmeermans@live.nl>
2017-05-09 13:28:54 +0200

    Setup the framework code for the retrieving robot system

--- src/RobotClasses/Robot.java
+++ src/RobotClasses/Robot.java
@@ -1,4 +1,33 @@
 package RobotClasses;
 
+import Warehouse.Location;
+import Warehouse.Product;
+
 public class Robot {
+    private Location locationRobot;
+
+    public Robot(Location locationRobot) {
+        this.locationRobot = locationRobot;
+    }
+
+    public Location getLocationRobot() {
+        return locationRobot;
+    }
+
+    public void moveRobot(Product[] ps){
+        for(Product p : ps){
+            while(getLocationRobot() != p.getLocation()){
+                //execute algorithm
+            }
+            if(getLocationRobot() == p.getLocation()){
+                retrieveProduct(p);
+            }
+        }
+    }
+
+    public void retrieveProduct(Product p){
+        if (p.getLocation().getX() == getLocationRobot().getX() && p.getLocation().getY() == getLocationRobot().getY()){
+            //execute arduino code for moving robot arm
+        }
+    }
 }
