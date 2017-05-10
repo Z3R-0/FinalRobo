89fd4a04860e0897998e74bcca6dc82509559e83
Erwin Jacobs <erwinpj@outlook.com>
2017-05-10 13:41:15 +0200

    minor edits

--- src/OrderInfo/Customer.java
+++ src/OrderInfo/Customer.java
@@ -7,14 +7,14 @@ public class Customer {
 	private String adress;
 	private String zipCode;
 	private String city;
-    
-    //CONSTRUCTOR
+
+	// CONSTRUCTOR
 	public Customer(String surName, String lastName, String adress, String zipCode, String city) {
 		this.surName = surName;
 		this.lastName = lastName;
 		this.adress = adress;
 		this.zipCode = zipCode;
 		this.city = city;
-	}	
+	}
 
 }
--- src/OrderInfo/Order.java
+++ src/OrderInfo/Order.java
@@ -4,34 +4,30 @@ import java.awt.List;
 import java.util.ArrayList;
 import java.util.Date;
 
-
 public class Order {
-    private int ordernumber;
-    private Date date;
-    private Customer customer;
- ArrayList<Integer> products = new ArrayList<Integer>();  
-    
-        //CONSTRUCTOR
-    public Order(int ordernumber, Date date, Customer customer) {
-        this.ordernumber = ordernumber;
-        this.date = date;
-        this.customer = customer;
-    }
+	private int ordernumber;
+	private Date date;
+	private Customer customer;
+	ArrayList<Integer> products = new ArrayList<Integer>();
 
+	// CONSTRUCTOR
+	public Order(int ordernumber, Date date, Customer customer) {
+		this.ordernumber = ordernumber;
+		this.date = date;
+		this.customer = customer;
+	}
 
-    //SETTERS
-    public void setOrdernummer(int ordernumber) {
-        this.ordernumber = ordernumber;
-    }
+	// SETTERS
+	public void setOrdernummer(int ordernumber) {
+		this.ordernumber = ordernumber;
+	}
 
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
+	public void setDate(Date date) {
+		this.date = date;
+	}
 
 	public void addProducts(int product) {
 		products.add(product);
 	}
-
 
 }
