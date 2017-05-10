0ee2e718be2994931b63e671a59eb59f7ec745f0
TimM <timmeermans@live.nl>
2017-05-10 13:13:33 +0200

    Updated application GUI. Done with designing now to fill in the functionality

--- src/RobotoAppMain/Controller2.java
+++ src/RobotoAppMain/Controller2.java
@@ -1,43 +1,40 @@
 package RobotoAppMain;
 
-import javafx.fxml.*;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
+public class Controller2 implements Initializable {
     @FXML
-    private Button fxButtonAdd;
+    private BorderPane mainPanePacking;
     @FXML
-    private BorderPane mainPaneRetrieve;
+    private TextArea fxTextAreaStatusPacking;
     @FXML
-    private TextArea fxTextAreaStatusRetrieve;
+    private Button fxButtonAlgorithm1;
     @FXML
-    private Button fxButtonExecute;
-    @FXML
-    private Button fxButtonPacking;
+    private Button fxButtonRetrieve;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        fxButtonAdd.setOnAction((event) -> {
-            fxTextAreaStatusRetrieve.appendText("Bestand toegevoegd!\n");
+        fxButtonAlgorithm1.setOnAction((event) -> {
+            fxTextAreaStatusPacking.appendText("Algoritme1 uitgevoerd!\n");
         });
 
-        fxButtonExecute.setOnAction((event) -> {
-            fxTextAreaStatusRetrieve.appendText("Order uitgevoerd!\n");
-        });
-
-        fxButtonPacking.setOnAction((event) -> {
+        fxButtonRetrieve.setOnAction((event) -> {
             Parent root;
             try {
-                root = FXMLLoader.load(Controller2.class.getResource("mainLayoutPacking.fxml"), resources);
+                root = FXMLLoader.load(Controller.class.getResource("mainLayoutRetrieve.fxml"), resources);
                 Stage stage = new Stage();
                 stage.setTitle("Magazijnrobot");
                 stage.setScene(new Scene(root, 1820, 980));
--- src/RobotoAppMain/Main.java
+++ src/RobotoAppMain/Main.java
@@ -2,23 +2,19 @@ package RobotoAppMain;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.*;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
-
-import javax.swing.*;
 
 public class Main extends Application {
     private Stage primaryStage;
     private BorderPane mainLayout;
 
-
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("mainLayoutRetrieve.fxml"));
         primaryStage.setTitle("Magazijnrobot");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, 1820, 980));
         primaryStage.show();
         primaryStage.setResizable(false);
     }
