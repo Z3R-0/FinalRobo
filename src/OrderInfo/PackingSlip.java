a226ce2544eac446051c4cb731692e75439db4fe
Erwin Jacobs <erwinpj@outlook.com>
2017-05-12 11:13:38 +0200

    tostring voor location

--- src/OrderInfo/PackingSlip.java
+++ src/OrderInfo/PackingSlip.java
@@ -2,4 +2,9 @@ package OrderInfo;
 
 public class PackingSlip {
 	
+	public void printSlip(){
+		for (){
+			System.out.println();
+		}
+	}
 }
--- src/Warehouse/Location.java
+++ src/Warehouse/Location.java
@@ -1,32 +1,36 @@
 package Warehouse;
 
 public class Location {
-    //VARIABLES
-    private int x;
-    private int y;
+	// VARIABLES
+	private int x;
+	private int y;
 
-    //CONSTRUCTOR
-    public Location(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
+	// CONSTRUCTOR
+	public Location(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
 
-    //FUNCTIONS
-    public int getX() {
-        return x;
-    }
+	// FUNCTIONS
+	public int getX() {
+		return x;
+	}
 
-    public int getY() {
-        return y;
-    }
+	public int getY() {
+		return y;
+	}
 
-    public double getLengte(Location location1) {
-        return 2.0;
-    }
+	public double getLengte(Location location1) {
+		return 2.0;
+	}
 
-    public static double getLengte(Location Locatie1, Location Locatie2) {
-        int xAfstand = Locatie1.getX() - Locatie2.getX();
-        int yAfstand = Locatie1.getY() - Locatie2.getY();
-        return Math.sqrt(Math.pow(xAfstand, 2) + Math.pow(yAfstand, 2));
-    }
+	public static double getLengte(Location Locatie1, Location Locatie2) {
+		int xAfstand = Locatie1.getX() - Locatie2.getX();
+		int yAfstand = Locatie1.getY() - Locatie2.getY();
+		return Math.sqrt(Math.pow(xAfstand, 2) + Math.pow(yAfstand, 2));
+	}
+
+	public String toString() {
+		return "[" + getX() + ", " + getY() + "]";
+	}
 }
